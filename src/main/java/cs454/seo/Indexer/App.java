4f4038ca39f9bffc44168b002e8a1b48d4c00ec2
Gaurav Sisodiya <ggsisodia147@gmail.com>
2015-03-13 01:20:42 -0700

    Initial Commit.

---
+++ src/main/java/cs454/seo/Indexer/App.java
@@ -0,0 +1,13 @@
+package cs454.seo.Indexer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
